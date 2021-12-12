db4c22564a77f212fa466ada8eabf34626389017
LuiggiPasache <luiggi.pasache.lopera@gmail.com>
2021-12-12 11:06:09 -0500

    quitando conexion a bd

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var database = require('../../database/database');
+// var database = require('../../database/database');
 
-/* GET users listing. */
+/*
+// GET users listing.
 router.get('/', function (req, res, next) {
   database.query('SELECT * FROM users', function (err, rows, fields) {
     if (err) { console.log(err); }
     res.send(rows);
   });
 });
+*/
 
 module.exports = router;
