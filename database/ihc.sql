22d89b4b10329ea602c05e1971efa9b7272ffc13
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2021-12-24 12:49:50 -0500

    Agregando datos a base de datos

--- database/ihc.sql
+++ database/ihc.sql
@@ -66,3 +66,31 @@ CREATE TABLE specialities (
     FOREIGN KEY (doctor_id) REFERENCES doctors(id)
 );
 
+-- inserciones
+insert into doctors (first_name, last_name) values ('Merla', 'Havvock');
+insert into doctors (first_name, last_name) values ('Lindsay', 'Tregunnah');
+insert into doctors (first_name, last_name) values ('Andrus', 'Nannini');
+insert into doctors (first_name, last_name) values ('Issi', 'Lippiello');
+insert into doctors (first_name, last_name) values ('Nerty', 'Ree');
+insert into doctors (first_name, last_name) values ('Nicol', 'Escudier');
+insert into doctors (first_name, last_name) values ('Hal', 'Iacovino');
+insert into doctors (first_name, last_name) values ('Charmine', 'Keilty');
+insert into doctors (first_name, last_name) values ('Petrina', 'Boughtflower');
+insert into doctors (first_name, last_name) values ('Coral', 'Massenhove');
+insert into doctors (first_name, last_name) values ('Raphaela', 'Cottell');
+insert into doctors (first_name, last_name) values ('Ondrea', 'Acum');
+insert into doctors (first_name, last_name) values ('Leila', 'Adrain');
+insert into doctors (first_name, last_name) values ('Judye', 'Capehorn');
+insert into doctors (first_name, last_name) values ('Klement', 'Sleeny');
+insert into doctors (first_name, last_name) values ('Klarika', 'Maudsley');
+insert into doctors (first_name, last_name) values ('Trever', 'Antonescu');
+insert into doctors (first_name, last_name) values ('Faydra', 'McLewd');
+insert into doctors (first_name, last_name) values ('Onida', 'Giraudoux');
+insert into doctors (first_name, last_name) values ('Corry', 'Gosselin');
+insert into doctors (first_name, last_name) values ('Hyman', 'Coppin');
+insert into doctors (first_name, last_name) values ('Robby', 'Tombleson');
+insert into doctors (first_name, last_name) values ('Elaine', 'Dossetter');
+
+insert into diseases (name) values ('Bronquitis aguda'), ('Sinusitis'), ('Infección urinaria');
+
+insert into users (unsername, password) values ('admin', '123456');
