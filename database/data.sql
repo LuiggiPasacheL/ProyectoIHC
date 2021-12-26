a449f0cc937cacb95da51daf88bccf79c6c20b59
Yanfer <oscar.yanfer@unmsm.edu.pe>
2021-12-26 14:59:12 -0500

    avance

--- database/data.sql
+++ database/data.sql
@@ -1,13 +1,9 @@
 
 -- inserciones
 insert into doctors (first_name, last_name) values ('Merla', 'Havvock');
-insert into specialities (name, doctor_id) values ('Cardiologia', 1);
 insert into doctors (first_name, last_name) values ('Lindsay', 'Tregunnah');
-insert into specialities (name, doctor_id) values ('Cardiologia', 2);
 insert into doctors (first_name, last_name) values ('Andrus', 'Nannini');
-insert into specialities (name, doctor_id) values ('Pediatria', 3);
 insert into doctors (first_name, last_name) values ('Issi', 'Lippiello');
-insert into specialities (name, doctor_id) values ('Medicina interna', 4);
 insert into doctors (first_name, last_name) values ('Nerty', 'Ree');
 insert into doctors (first_name, last_name) values ('Nicol', 'Escudier');
 insert into doctors (first_name, last_name) values ('Hal', 'Iacovino');
@@ -27,7 +23,30 @@ insert into doctors (first_name, last_name) values ('Corry', 'Gosselin');
 insert into doctors (first_name, last_name) values ('Hyman', 'Coppin');
 insert into doctors (first_name, last_name) values ('Robby', 'Tombleson');
 insert into doctors (first_name, last_name) values ('Elaine', 'Dossetter');
+insert into doctors (first_name, last_name) values ('Vladimir', 'Cerron');
+insert into doctors (first_name, last_name) values ('Cesar', 'Hinostroza');
 
-insert into diseases (name) values ('Bronquitis aguda'), ('Sinusitis'), ('Infección urinaria');
+insert into diseases (name) values ('Bronquitis aguda'), ('Sinusitis'), ('Infección urinaria'), ('Asma'), ('Neumonia');
 
 insert into users (username, password) values ('admin', '123456');
+
+insert into specialities (name, doctor_id) values ('Cardiologia', 1);
+insert into specialities (name, doctor_id) values ('Cardiologia', 2);
+insert into specialities (name, doctor_id) values ('Cardiologia', 3);
+insert into specialities (name, doctor_id) values ('Cardiologia', 4);
+insert into specialities (name, doctor_id) values ('Cardiologia', 5);
+insert into specialities (name, doctor_id) values ('Pediatria', 6);
+insert into specialities (name, doctor_id) values ('Pediatria', 7);
+insert into specialities (name, doctor_id) values ('Pediatria', 8);
+insert into specialities (name, doctor_id) values ('Pediatria', 9);
+insert into specialities (name, doctor_id) values ('Pediatria', 10);
+insert into specialities (name, doctor_id) values ('Medicina interna', 11);
+insert into specialities (name, doctor_id) values ('Medicina interna', 12);
+insert into specialities (name, doctor_id) values ('Medicina interna', 13);
+insert into specialities (name, doctor_id) values ('Medicina interna', 14);
+insert into specialities (name, doctor_id) values ('Medicina interna', 15);
+insert into specialities (name, doctor_id) values ('Dermatología', 16);
+insert into specialities (name, doctor_id) values ('Dermatología', 17);
+insert into specialities (name, doctor_id) values ('Dermatología', 18);
+insert into specialities (name, doctor_id) values ('Dermatología', 19);
+insert into specialities (name, doctor_id) values ('Dermatología', 20);
